065dfaca0ffe9792a831f3bfdf6419f5db2fc3c9
KamiGasai <123494249@qq.com>
2018-03-10 21:21:52 -0800

    First try on creating an easy TableView with contents of fps games

--- tableView/tableView/tableViewController.swift
+++ tableView/tableView/tableViewController.swift
@@ -11,4 +11,21 @@ import UIKit
 
 class tableViewController: UITableViewController {
     
+    var fps_game: [String] = ["CounterStrike", "RainbowSix", "PUBG"]
+    
+    //this method return the length of the array, indicates how many elements in our array (are going to be shown)
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fps_game.count
+    }
+    //this method actually create(return) a cell (a view)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let current_game = fps_game[indexPath.row]
+        //we actually know cell will be there, so it is good for us to force wrapping it
+        let cell = tableView.dequeueReusableCell(withIdentifier: "fpsCell")!
+        cell.textLabel?.text = current_game
+        
+        return cell //the cell has been wrapped, so it can be returned as a UITableViewCell
+        
+    }
 }
